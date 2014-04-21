51f12489a08c02878472ac483ff8207ffe5e16a2
Will Buck <wcbuck@gmail.com>
2014-04-21 10:49:01 -0400

    allow multiple files to be appended
    use do.call function to take a list of frames an combine them

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,25 +1,30 @@
-
-pollutantmean <- function(directory, pollutant,id = 1:332) {
+pollutantmean <- function(directory, pollutant, id = 1:332) {
 	
+## move working directory to one up from the 'specdata' directory
+
 	setwd("c:/Users/wbuck/Desktop/R_stuff/Coursera_R")
 
-#get a list of files in the specdata directory
+## get a list of files in the specdata directory
 
 	setwd(directory)
 	b <- dir()
+  myfiles  <- b[id]
 
-#load the appropriate csv file
-	classes <- c("factor","num","num","int")
-	x <- read.csv(b[id])
-#	x <- read.csv(b[id], colclasses= classes)
+## load the appropriate csv files
+	xlist <- lapply(myfiles, read.csv)
+ 	x  <- do.call("rbind", xlist)
 
-#filter out nulls
+##	x <- read.csv(myfiles)
+##	classes <- c("factor","num","num","int")
+##	x <- read.csv(myfiles, colClasses= classes)
+
+## find nulls
 	y <- !is.na(x[pollutant])
 
-#subset the data to focus on the required pollutant
+## subset the data to focus on the required pollutant
 	z <- x[pollutant]
 
-#then get the mean
+## then get the mean
 	mean(z[y])
 
 }
